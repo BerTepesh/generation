d7e9a88e83f7732e87adb4e5702530dea05541b2
BerTepesh <Ber.Tepesh@gmail.com>
2020-10-03 20:06:19 +0300

    anim done

--- js/main.js
+++ js/main.js
@@ -65,11 +65,13 @@ class Flow {
   path = [];
   parrentState;
   isMove = true;
-  step;
+  velocity;
+  tick;
 
-  constructor(_path, _step) {
+  constructor(_path, _velocity) {
     this.path = _path;
-    this.step = _step;
+    this.velocity = 100/_velocity;
+    this.tick = this.velocity;
   }
   addSprite = (_sprite) => {
     if(_sprite) {
@@ -91,41 +93,42 @@ class Flow {
       this.sprites.forEach((e, index) => {
         flowItem.innerHTML += '<span class="' +  e.icon + '" id="' +  this.id + '_' + index + '"></span>';
         e.id = this.id + '_' + index;
-
-        if(second.x != 0 && second.y != 0) {
-          alert('Задан некорректный путь!');
-        } else if(first.x > second.x) {
-          if(this.sprites[index + 1]) {
-            this.sprites[index + 1].x += e.gutter;
-          } else {
+        if(first.x > second.x) {
+          if(index == 0) {
             e.x = -e.gutter;
+          } else {
+            e.x = (e.gutter * (index - 1));
           }
+          e.y = this.path[0].y;
+          e.curDirection = 2; 
           e.direction = 2; 
-          e.el().style.transform = "translate(" + e.x + "px, 0) scale(-1, 1)";
         } else if(first.x < second.x) {
-          if(this.sprites[index + 1]) {
-            this.sprites[index + 1].x -= e.gutter;
-          } else {
+          if(index == 0) {
             e.x = e.gutter;
+          } else {
+            e.x = -(e.gutter * (index - 1));
           }
+          e.y = this.path[0].y;
+          e.curDirection = 0; 
           e.direction = 0; 
-          e.el().style.transform = "translate(" + e.x + "px, 0)";
         } else if(first.y > second.y) {
-          if(this.sprites[index + 1]) {
-            this.sprites[index + 1].y += e.gutter;
-          } else {
-            e.y = -e.gutter;
-          }
-          e.direction = 3; 
-          e.el().style.transform = "translate(0, " + e.y + "px) rotate(-90deg)";
-        } else if(first.y < second.y) {
-          if(this.sprites[index + 1]) {
-            this.sprites[index + 1].y -= e.gutter;
-          } else {
+          if(index == 0) {
             e.y = e.gutter;
+          } else {
+            e.y = (e.gutter * (index - 1));
           }
+          e.x = this.path[0].x;
+          e.curDirection = 3; 
+          e.direction = 3; 
+        } else if(first.y < second.y) {
+          if(index == 0) {
+            e.y = e.gutter;
+          } else {
+            e.y = -(e.gutter * (index - 1));
+          }
+          e.x = first.x;
+          e.curDirection = 1; 
           e.direction = 1; 
-          e.el().style.transform = "translate(0, " + e.y + "px) rotate(90deg)";
         } 
       });
     }
@@ -156,67 +159,78 @@ class Flow {
       _el.el().style.transform = "translate(" + _el.x + "px, " + _el.y + "px) rotate(" + _rotate + "deg)";
     }
   }
- 
   move = () => {
-    if(this) { //.isMove
-      this.sprites.forEach(el => {
+    if(this.tick <= 0) {
+      this.sprites.forEach((el, index) => {
         if(!this.getNextPathStep(el)) {
           this.isMove = false;
+          el.pathStep = 0;
+          el.x = this.path[0].x;
+          el.y = this.path[0].y;
+          el.curDirection = el.direction;
         } else {
-          if(el.direction == 0) {
+          if(el.curDirection == 0) {
             if(this.getNextPathStep(el).x >= el.x) {
-              this.#transform(el);
+              if(el.x >= 0) {           // Чтоб начинать от state
+                this.#transform(el);    
+              }    
             } else {
               el.pathStep++;
               if(this.getNextPathStep(el).y >= el.y) {
-                el.direction = 1;
+                el.curDirection = 1;
               } else {
-                el.direction = 3;
+                el.curDirection = 3;
               }
             }
-            el.x += this.step;
-          } else if(el.direction == 1) {
+            el.x++;
+          } else if(el.curDirection == 1) {
             if(this.getNextPathStep(el).y >= el.y) {
-              this.#transform(el, 90);
+              if(el.y >= 0) {           // Чтоб начинать от state
+                this.#transform(el, 90);  
+              } 
             } else {
               el.pathStep++;
               if(this.getNextPathStep(el).x >= el.x) {
-                el.direction = 0;
+                el.curDirection = 0;
               } else {
-                el.direction = 2;
+                el.curDirection = 2;
               }
             }
-            el.y += this.step;
-          } else if(el.direction == 2) {
+            el.y++;
+          } else if(el.curDirection == 2) {
             if(this.getNextPathStep(el).x <= el.x) {
-              this.#transform(el, 180);
+              if(el.x < 0) {           // Чтоб начинать от state
+                this.#transform(el, 180);  
+              }  
             } else {
               el.pathStep++;
               if(this.getNextPathStep(el).y <= el.y) {
-                el.direction = 3;
+                el.curDirection = 3;
               } else {
-                el.direction = 1;
+                el.curDirection = 1;
               }
             }
-            el.x -= this.step;
-          } else if(el.direction == 3) {
+            el.x--;
+          } else if(el.curDirection == 3) {
             if(this.getNextPathStep(el).y <= el.y) {
-              this.#transform(el, 270);
+              if(el.y < 0) {           // Чтоб начинать от state
+                this.#transform(el, 270);  
+              } 
             } else {
               el.pathStep++;
               if(this.getNextPathStep(el).x <= el.x) {
-                el.direction = 2;
+                el.curDirection = 2;
               } else {
-                el.direction = 0;
+                el.curDirection = 0;
               }
             }
-            el.y -= this.step;
+            el.y--;
           }
         }
       });
-    } else {
-      return false;
-    }
+      this.tick = this.velocity;
+    } 
+    this.tick--;
   }
 }
 class Sprite {
@@ -225,6 +239,7 @@ class Sprite {
   gutter;
   split;
   pathStep = 0;
+  curDirection;
   direction;
   x = 0;
   y = 0;
@@ -237,205 +252,197 @@ class Sprite {
     return document.getElementById(this.id);
   }
 }
-const reciclePath1 = [
+const reciclePath = [
   {
     x: 0,
     y: 0
   },{
-    x: 150,
-    y: 0
-  },{
-    x: 150,
-    y: 150
-  },{
-    x: 0,
-    y: 150
-  },{
-    x: 0,
+    x: 208,
     y: 0
   }
 ];
-const reciclePath12 = [
+const wastePath = [
+  {
+    x: 0,
+    y: 0
+  },{
+    x: 144,
+    y: 0
+  },{
+    x: 144,
+    y: 205
+  }
+];
+const couplesPath = [
+  {
+    x: 19,
+    y: 0
+  },{
+    x: 19,
+    y: 99
+  },{
+    x: 157,
+    y: 99
+  },{
+    x: 157,
+    y: 170
+  }
+];
+const gasPath = [
+  {
+    x: 21,
+    y: 0
+  },{
+    x: 21,
+    y: -205
+  },{
+    x: 160,
+    y: -205
+  }
+];
+const condensatePath = [
   {
     x: 0,
     y: 0
   },{
     x: 0,
-    y: 100
+    y: -96
   },{
-    x: 100,
-    y: 100
+    x: 135,
+    y: -96
   },{
-    x: 100,
+    x: 135,
+    y: -155
+  }
+];
+const couplesPath2 = [
+  {
+    x: 0,
     y: 0
   },{
-    x: 0,
+    x: -275,
     y: 0
   }
 ];
-const reciclePath2 = [
+const electricityPath = [
   {
     x: 0,
     y: 0
   },{
-    x: 0,
-    y: 100
-  },{
-    x: -100,
-    y: 100
-  },{
-    x: -100,
-    y: 0
-  },{
-    x: 0,
+    x: 195,
     y: 0
   }
 ];
-const reciclePath22 = [
+const ashPath = [
   {
     x: 0,
-    y: 0
+    y: -13
   },{
-    x: -100,
-    y: 0
-  },{
-    x: -100,
-    y: 100
-  },{
-    x: 0,
-    y: 100
-  },{
-    x: 0,
-    y: 0
+    x: 195,
+    y: -13
   }
 ];
-const reciclePath3 = [
+const slagPath = [
   {
     x: 0,
     y: 0
   },{
-    x: -100,
-    y: 0
-  },{
-    x: -100,
-    y: -100
-  },{
-    x: 0,
-    y: -100
-  },{
-    x: 0,
-    y: 0
-  }
-];
-const reciclePath32 = [
-  {
-    x: 0,
-    y: 0
-  },{
-    x: 0,
-    y: -100
-  },{
-    x: -100,
-    y: -100
-  },{
-    x: -100,
-    y: 0
-  },{
-    x: 0,
-    y: 0
-  }
-];
-const reciclePath4 = [
-  {
-    x: 0,
-    y: 0
-  },{
-    x: 0,
-    y: -100
-  },{
-    x: 100,
-    y: -100
-  },{
-    x: 100,
-    y: 0
-  },{
-    x: 0,
-    y: 0
-  }
-];
-const reciclePath42 = [
-  {
-    x: 0,
-    y: 0
-  },{
-    x: 100,
-    y: 0
-  },{
-    x: 100,
-    y: -100
-  },{
-    x: 0,
-    y: -100
-  },{
-    x: 0,
+    x: 350,
     y: 0
   }
 ];
 
 
 // Создание потока
-const recicleFlow1 = new Flow(reciclePath1, 2);
-const recicleFlow12 = new Flow(reciclePath12, 2);
-
-const recicleFlow2 = new Flow(reciclePath2, 2);
-const recicleFlow22 = new Flow(reciclePath22, 2);
-
-const recicleFlow3 = new Flow(reciclePath3, 2);
-const recicleFlow32 = new Flow(reciclePath32, 2);
-
-const recicleFlow4 = new Flow(reciclePath4, 2);
-const recicleFlow42 = new Flow(reciclePath42, 2);
+const recicleFlow = new Flow(reciclePath, 70); // скорость (0-100)
+const wasteFlow = new Flow(wastePath, 60);
+const couplesFlow = new Flow(couplesPath, 50);
+const gasFlow = new Flow(gasPath, 40);
+const condensateFlow = new Flow(condensatePath, 30);
+const couplesFlow2 = new Flow(couplesPath2, 20);
+const electricityFlow = new Flow(electricityPath, 10);
+const ashFlow = new Flow(ashPath, 30);
+const slagFlow = new Flow(slagPath, 40);
 
 // Наполнение потока спрайтами
-for(let i = 0; i < 3; i++) {
+for(let i = 0; i < 7; i++) {
   // if(i % 3) {
   //   recicleFlow.addSprite(new Sprite('icon-smoke', 20, false));
   // } else {
   //   recicleFlow.addSprite(new Sprite('icon-smoke', 20, true));
   // }
-
-  recicleFlow1.addSprite(new Sprite('icon-smoke', 30, false));
-  recicleFlow12.addSprite(new Sprite('icon-smoke', 20, false));
-  recicleFlow2.addSprite(new Sprite('icon-smoke', 20, false));
-  recicleFlow22.addSprite(new Sprite('icon-smoke', 20, false));
-  recicleFlow3.addSprite(new Sprite('icon-smoke', 20, false));
-  recicleFlow32.addSprite(new Sprite('icon-smoke', 20, false));
-  recicleFlow4.addSprite(new Sprite('icon-smoke', 20, false));
-  recicleFlow42.addSprite(new Sprite('icon-smoke', 20, false));
+  recicleFlow.addSprite(new Sprite('icon-recicle-flow', 30, false));
+}
+for(let i = 0; i < 8; i++) {
+  // if(i % 3) {
+  //   recicleFlow.addSprite(new Sprite('icon-smoke', 20, false));
+  // } else {
+  //   recicleFlow.addSprite(new Sprite('icon-smoke', 20, true));
+  // }
+  couplesFlow.addSprite(new Sprite('icon-couples', 40, false));
+  couplesFlow2.addSprite(new Sprite('icon-couples', 35, false));
+  electricityFlow.addSprite(new Sprite('icon-flash-other', 25, false));
+  ashFlow.addSprite(new Sprite('icon-filter-tube', 25, false));
+}
+for(let i = 0; i < 9; i++) {
+  // if(i % 3) {
+  //   recicleFlow.addSprite(new Sprite('icon-smoke', 20, false));
+  // } else {
+  //   recicleFlow.addSprite(new Sprite('icon-smoke', 20, true));
+  // }
+  wasteFlow.addSprite(new Sprite('icon-recicle-flow', 40, false));
+  gasFlow.addSprite(new Sprite('icon-smoke', 45, false));
+}
+for(let i = 0; i < 10; i++) {
+  // if(i % 3) {
+  //   recicleFlow.addSprite(new Sprite('icon-smoke', 20, false));
+  // } else {
+  //   recicleFlow.addSprite(new Sprite('icon-smoke', 20, true));
+  // }
+  slagFlow.addSprite(new Sprite('icon-burn-flow', 35, false));
+}
+for(let i = 0; i < 11; i++) {
+  // if(i % 3) {
+  //   recicleFlow.addSprite(new Sprite('icon-smoke', 20, false));
+  // } else {
+  //   recicleFlow.addSprite(new Sprite('icon-smoke', 20, true));
+  // }
+  condensateFlow.addSprite(new Sprite('icon-condensate', 30, false));
 }
 
 // Создание позиции
 const recicleState = new State('recicle');
+const bunkerState = new State('bunker');
+const burnState = new State('burn');
+const capacitorState = new State('capacitor');
+const turboState = new State('turbo');
+const filterState = new State('filter');
 
 // Добавление потока в позицию
-recicleState.addFlow(recicleFlow1);
-recicleState.addFlow(recicleFlow12);
-recicleState.addFlow(recicleFlow2);
-recicleState.addFlow(recicleFlow22);
-recicleState.addFlow(recicleFlow3);
-recicleState.addFlow(recicleFlow32);
-recicleState.addFlow(recicleFlow4);
-recicleState.addFlow(recicleFlow42);
+recicleState.addFlow(recicleFlow);
+bunkerState.addFlow(wasteFlow);
+burnState.addFlow(couplesFlow);
+burnState.addFlow(gasFlow);
+burnState.addFlow(slagFlow);
+capacitorState.addFlow(condensateFlow);
+turboState.addFlow(couplesFlow2);
+turboState.addFlow(electricityFlow);
+filterState.addFlow(ashFlow);
 
 // Создание движка
 const engine = new Engine();
 
 // Доавление позиции в движок
 engine.addState(recicleState);
+engine.addState(bunkerState);
+engine.addState(burnState);
+engine.addState(capacitorState);
+engine.addState(turboState);
+engine.addState(filterState);
 
 // Инициализация движка
 engine.init();
 
-// Рендер (скорость 0 - 10)
-//engine.render(3);
+// Рендер (скорость 0 - 100)
+engine.render(50);
 
